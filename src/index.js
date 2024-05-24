2fba497b7155a2e50dd36e37870ecfc58b78db8c
Anish <anishd988@gmail.com>
2024-05-24 19:25:53 +0530

    connected the database

--- src/index.js
+++ src/index.js
@@ -1,1 +1,53 @@
-console.log('Hello, world!');
+// require("dotenv").config({ path: "./.env" }); since it breaks the consistency of the codebase.
+// we use import instead of require in the codebase.
+import dotenv from "dotenv";
+
+
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+// import express from "express";
+// const app = express();
+import connectDB from "./db/index.js";
+
+
+dotenv.config({ path: "./.env" });
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+(async ()=>{
+    try{
+         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+         app.on("error",(error)=>{
+            console.log("Error",error)
+            throw error
+         })
+
+         app.listen(process.env.PORT,()=>{
+            console.log(`Server is running on port ${process.env.PORT}`)
+         })
+    }
+    catch(e){
+        console.log("Error",e)
+        throw e
+    }
+})()
+*/
